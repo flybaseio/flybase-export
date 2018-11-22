c728728f6ffce18b6c7d96f69af7d2fac7dd1ab5
Roger <roger@datamcfly.com>
2018-11-22 17:33:59 +0000

    first

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# flybase-export
-Node module for exporting large data from Flybase
+# Flybase-Export
+
+Flybase-export is a helper utility for export large JSON files into [Flybase](https://www.flybase.io/). It
+breaks the JSON into smaller chunks and uploads them individually through the Flybase API.
+
+## Installing
+
+Install the flybase-export module globally:
+
+    $ npm install -g flybase-export
+
+or install it locally and add it to your path:
+
+    $ npm install flybase-export
+    $ export PATH=$PATH:`npm bin`
+
+## Usage
+
+    $ flybase-export
+    Usage: flybase-export
+
+    Options:
+      --flybase_key, -k          Flybase API Key.                                                          [required]
+      --flybase_app, -a          Flybase App name.                                                         [required]
+      --flybase_collection, -c   Flybase collection name.                                                  [required]
+      --json, -j                 The JSON file to export.                                                  [required]
+
+
+## Example
+
+    $ flybase-export --flybase_key API-KEY --flybase-app Test --flybase_collection Stuff --json test.json
+    exporting [=================================================] 100% (9431/9431)
+    export completed.
---
+++ bin/flybase-export.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+var flybase = require('flybase'),
+	optimist = require('optimist'),
+	ProgressBar = require('progress'),
+	assert = require('assert'),
+	path = require('path'),
+	fs = require('fs'),
+	util = require('util');
+
+// We try to write data in ~1MB chunks (in reality this often ends up being much smaller, due to the JSON structure).
+var CHUNK_SIZE = 1024*1024;
+
+// Keep ~50 writes outstanding at a time (this increases throughput, so we're not delayed by server round-trips).
+var OUTSTANDING_WRITE_COUNT = 50;
+
+var argv = require('optimist')
+	.usage('Usage: $0')
+
+	.demand('flybase_key')
+	.describe('flybase_key', 'Flybase API Key.')
+	.alias('k', 'flybase_key')
+
+	.demand('flybase_app')
+	.describe('flybase_app', 'Flybase App Name.')
+	.alias('a', 'flybase_app')
+
+	.demand('flybase_collection')
+	.describe('flybase_collection', 'Flybase Collection Name.')
+	.alias('c', 'flybase_collection')
+
+	.demand('json')
+	.describe('json', 'The JSON file to export to.')
+	.alias('j', 'json')
+
+	.argv;
+
+async function main() {
+	console.log( argv );
+	const file = path.resolve(argv.json);
+	let data = []
+	const flybaseRef = flybase.init(argv.flybase_app, argv.flybase_collection, argv.flybase_key );
+	flybaseRef.on("connect", async function (data ){
+		try {
+			const snapshot = await flybaseRef.once('value');
+			await snapshot.forEach( function (row){
+				console.log( row.value() )
+				data.push( row.value() );
+			});
+			console.log("done");
+			fs.writeFile (file, JSON.stringify(data), function(err) {
+				if (err) throw err;
+				console.log('complete');
+				process.exit();
+			});
+		} catch(error) {
+			console.error(error);
+		}
+	});
+}
+
+main();
